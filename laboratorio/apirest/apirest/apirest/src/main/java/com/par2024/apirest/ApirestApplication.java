c9a6e04348bb54c669184c9706e090ef232079b3
Matias Meza <matsosa55@gmail.com>
2025-03-23 20:54:50 -0300

    Unidad 2 / Unidad 3

---
+++ laboratorio/apirest/apirest/apirest/src/main/java/com/par2024/apirest/ApirestApplication.java
@@ -0,0 +1,13 @@
+package com.par2024.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
